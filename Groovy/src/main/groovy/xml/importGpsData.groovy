570f03ad27e0d9373a3257bb15f58393a3b8f0eb
Viktoriia <34584226+abakumova@users.noreply.github.com>
2020-04-30 13:07:47 +0300

    Groovy (xml refactoring) (#9)

--- Groovy/src/main/groovy/xml/importGpsData.groovy
+++ Groovy/src/main/groovy/xml/importGpsData.groovy
@@ -3,10 +3,6 @@ package xml
 @Grapes(
         @Grab(group = 'joda-time', module = 'joda-time', version = '2.10.5')
 )
-import org.joda.time.DateTime
-@Grapes(
-        @Grab(group = 'joda-time', module = 'joda-time', version = '2.10.5')
-)
 
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
